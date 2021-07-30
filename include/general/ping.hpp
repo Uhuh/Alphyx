69f81a0b93b0c4bdba5bc1951cc1a1ca69f981da
Dylan Warren <dylanwarren19@gmail.com>
2021-07-30 01:50:10 -0500

    Try to setup ping slash command.

--- include/general/ping.hpp
+++ include/general/ping.hpp
@@ -2,6 +2,7 @@
 #define ALPHYX_PING_H
 
 #include "command.hpp"
+#include <dpp/message.h>
 
 class PingCommand: public Command {
   protected:
@@ -11,13 +12,33 @@ class PingCommand: public Command {
     ) const override {
       client->message(event, "Ping pong! I work.. I think?");
     }
+
+    void slash_command_create() {
+      dpp::slashcommand command;
+
+      command.set_name(this->m_name)
+        .set_description(this->m_description)
+        .set_application_id(this->client->cluster->me.id)
+        .add_option(
+          dpp::command_option(dpp::co_string, "ping", "The type of ping", true)
+            .add_choice(dpp::command_option_choice("Ping", std::string("normal_ping")))
+            .add_choice(dpp::command_option_choice("Pong", std::string("ping_pong")))
+            .add_choice(dpp::command_option_choice("Pyng", std::string("pyng")))
+        );
+
+      this->client->cluster->global_command_create(command);
+      Client::log(LogType::INFO, "Initialized Ping slash command.");
+    }
+
   public:
     explicit PingCommand(const Client *bot): Command(
       bot,
       CommandType::GENERAL,
       "ping",
       "stupid description"
-    ) {}
+    ) {
+      slash_command_create();
+    }
 };
 
 #endif
