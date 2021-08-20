26e7ee4476557fc379631dfab365d347cd22b1c6
Dylan Warren <dylanwarren19@gmail.com>
2021-08-19 22:53:22 -0500

    Replaced the basic message command for join roles, and now use a slash command which will always have values so should never fail.

--- src/general.cpp
+++ src/general.cpp
@@ -3,82 +3,89 @@
 #include <general/join-role.hpp>
 
 /**
- *
- * @param event Should have mentioned role or passed ID as well with saying what join event.
- * @param words AAAAAAAAAAAAA
+ * Add or remove a role to a guilds join list, or list the roles to the user.
+ * @param event Slash command with types and possible role id.
+ * @param name
  */
-void JoinRoleCommand::command_exec(const dpp::message_create_t &event, std::vector<std::string> &words) const {
-  if (words.empty()) {
-    client->message(event, "Hey listen! You forgot some parameters.", dpp::mt_reply);
-    return;
-  }
-
+void JoinRoleCommand::command_exec(const dpp::interaction_create_t &event) const {
+  dpp::command_interaction cmd_data = std::get<dpp::command_interaction>(event.command.data);
   /**
-   * Handle join operation(?) EG: add / remove / list
+   * This can be add / remove / list.
    */
+  std::string action = cmd_data.options.begin()->options.begin()->name;
 
-  uint64_t roleId;
-  std::string query = "SELECT role_id FROM join_roles WHERE guild_id=?";
-  // Must be add or remove
-  std::string type = words.front();
+  if (action == "add" || action == "remove") {
+    dpp::snowflake role_id = std::get<dpp::snowflake>(cmd_data.options.begin()->options.begin()->options.begin()->value);
+    std::string query = "INSERT INTO join_roles(role_id, guild_id) VALUES (?, ?)";
 
-  if (type != "add" && type != "remove") {
-    client->message(event, "You need to specify either `add` or `remove` for each role.");
-    return;
-  }
+    if (action == "remove") {
+      query = "DELETE FROM join_roles WHERE role_id=?";
+    }
 
-  if (!event.msg->mention_roles.empty()) {
-    roleId = event.msg->mention_roles.front();
-  } else {
-    // Remove the add/remove portion.
-    words.erase(words.begin());
+    sql::PreparedStatement *prep_stmt = client->con->prepareStatement(query);
+    prep_stmt->setBigInt(1, std::to_string(role_id));
+
+    // If we're adding a role then set the guild ID
+    if (action == "add") {
+      prep_stmt->setBigInt(2, std::to_string(event.command.guild_id));
+    }
+
+    // Wording :)
+    std::string response = "now";
+    if (action == "remove") {
+      response = "no longer";
+    }
 
     try {
-      // Cast user input to verify it's a real ID;
-      roleId = std::stoull(words.front());
+      prep_stmt->execute();
+      event.reply(
+        dpp::ir_channel_message_with_source,
+        dpp::message(fmt::format("Users will {} get <@&{}> when they join the server.", response, std::to_string(role_id)))
+          .set_flags(dpp::m_ephemeral)
+      );
+    } catch(std::exception & e) {
+      /**
+       * @TODO - Figure out how to parse error message. Assuming it only throws on duplicates right now
+       */
+      Client::log(LogType::ERROR, "User tried to add existing role id.");
 
-      dpp::cache *role_cache = dpp::get_role_cache();
-      dpp::cache_container &rc = role_cache->get_container();
-
-      if (rc.find(roleId) == rc.end()) {
-        client->message(event, "That doesn't appear to be a real role ID... try again.");
-        return;
-      }
-    } catch (const std::invalid_argument & e) {
-      Client::log(LogType::INFO, "User passed in a non uint type for role ID.");
-      client->message(event, "That's not a role ID!");
-      return;
+      event.reply(
+        dpp::ir_channel_message_with_source,
+        dpp::message("That role is already in the join role list. View the list by running `/join role list`")
+        .set_flags(dpp::m_ephemeral)
+      );
     }
+    return;
   }
+  else if (action == "list") {
+    client->stmt = client->con->createStatement();
+    client->res = client->stmt->executeQuery("SELECT role_id FROM join_roles WHERE guild_id="+std::to_string(event.command.guild_id));
 
-  // Prepare statements for following query
-  if (type == "add") {
-    query = "INSERT INTO join_roles(role_id, guild_id) VALUES (?, ?)";
-  } else if (type == "remove") {
-    query = "DELETE FROM join_roles WHERE role_id=?";
-  }
+    std::string rolesMentions;
 
-  sql::PreparedStatement *prep_stmt = client->con->prepareStatement(query);
-  prep_stmt->setBigInt(1, std::to_string(roleId));
+    // Make it so it mentions each role in the embed.
+    while(client->res->next()) {
+      rolesMentions += fmt::format("<@&{}>\n", client->res->getInt64(1));
+    }
 
-  if (type == "add") {
-    prep_stmt->setBigInt(2, std::to_string(event.msg->guild_id));
-  }
+    dpp::embed embed;
 
-  try {
-    prep_stmt->execute();
-  } catch(std::exception & e) {
-    /**
-     * @TODO - Figure out how to parse error message. Assuming it only throws on duplicates right now
-     */
-    Client::log(LogType::ERROR, "User tried to add existing role id.");
-    client->message(event, "That role is already in the join role list. View the list by running `rb join-role list`");
+    embed
+      .set_title("Roles given when users join")
+      .set_color(Colors::AQUA)
+      .set_description(rolesMentions);
+
+    event.reply(
+      dpp::ir_channel_message_with_source,
+      dpp::message(event.command.channel_id, embed).set_flags(dpp::m_ephemeral)
+    );
     return;
   }
 
-  /**
-   * @TODO - Implement listing join roles.
-   */
-
-  Client::log(LogType::SUCCESS, "Successfully executed " + type + " for guild[" + std::to_string(event.msg->guild_id) + "]");
+  // Users shouldn't be able to get here. If they do then wtf lol
+  event.reply(
+    dpp::ir_channel_message_with_source,
+    dpp::message("You shouldn't be able to see this.......")
+    .set_flags(dpp::m_ephemeral)
+  );
 }
