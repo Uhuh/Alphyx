3b216bda3419daf4a28c2c855c8d8708c5da6b12
Dylan Warren <dylanwarren19@gmail.com>
2021-11-29 19:21:12 -0600

    Give the bot a response document, check that both json files exist or the bot can't start.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,36 @@
 #include <bot.hpp>
+#include <database.h>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-  json configdocument;
+  srand(time(nullptr));
+
+  json configDocument;
   std::ifstream configfile("../config.json");
   configfile >> configdocument;
 
+  if (!configfile) {
+    Client::log(LogType::ERROR, "Couldn't find the config file, aborting boot up.");
+    return 1;
+  }
+
+  json responseDocument;
+  std::ifstream responseFile("../responses.json");
+  responsefile >> responsedocument;
+
+  if (!responsefile) {
+    Client::log(LogType::ERROR, "Couldn't find responses file, aborting boot up.");
+  }
+
+  /* Database stuff */
+  createAllTables(configdocument["postgresOptions"]);
+
   /* Login */
   dpp::cluster _bot(configdocument["token"]);
-  Client bot((std::shared_ptr<dpp::cluster>(&_bot)));
+  Client bot((std::shared_ptr<dpp::cluster>(&_bot)), configdocument, responsedocument);
 
   bot.cluster->start(false);
   return 0;
